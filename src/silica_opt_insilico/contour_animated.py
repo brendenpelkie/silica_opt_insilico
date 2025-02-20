1bcfc0f695ea8d1a3996b330947f7b0b22fd4c20
Brenden Pelkie <bgpelkie@gmail.com>
2025-02-19 17:08:46 -0800

    factor viz code in to modules

---
+++ src/silica_opt_insilico/contour_animated.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+
+class ContourAnimation:
+    def __init__(self, ammonia_range, water_range, teos_range, 
+                 Z_ammonia, Z_water, data, best_uuid, n_batches, 
+                 true_min, trial_name, n_grid, m_samples):
+        """
+        Initializes the animated contour plot.
+
+        Parameters:
+        - ammonia_range, water_range, teos_range: Value ranges for the contours.
+        - Z_ammonia, Z_water: Precomputed contour grid data.
+        - batches_points: List of batches (each containing points to plot).
+        - best_points: List of best points per batch.
+        - batch_names: List of batch names.
+        - true_min: True minimum point coordinates.
+        - trial_name: Title of the animation.
+        - n_batches: Number of batches.
+        - n_grid: Grid size for contours.
+        """
+        self.data = data
+        self.ammonia_range = ammonia_range
+        self.water_range = water_range
+        self.teos_range = teos_range
+        self.Z_ammonia = Z_ammonia
+        self.Z_water = Z_water
+        self.true_min = true_min
+        self.trial_name = trial_name
+        self.n_batches = n_batches
+        self.n_grid = n_grid
+        self.m_samples = m_samples
+        self.best_uuid = best_uuid
+        self.n_sobol = 2**m_samples
+
+
+
+        batch_nums = [str(i+1) for i in range(self.n_batches)]
+        self.batch_names = ['Sobol random']
+        self.batch_names.extend(batch_nums)
+
+
+        self.batches_points = self.get_batch_splits()
+        self.best_points = self.get_best_points()
+
+        self.num_frames = n_batches + 1  # Animation frames
+
+        # Create figure with two subplots
+        self.fig, self.ax = plt.subplots(1, 2, figsize=(13, 6))
+
+        # Plot Contour 1: TEOS vs. Water
+        contour1 = self.ax[0].contourf(self.teos_range, self.water_range, Z_ammonia, levels=20, cmap='viridis')
+        self.fig.colorbar(contour1, ax=self.ax[0], label='AP_distance')
+        self.ax[0].set_xlabel('TEOS')
+        self.ax[0].set_ylabel('Water')
+        self.ax[0].set_title('TEOS-Water')
+
+        # Plot Contour 2: TEOS vs. Ammonia
+        contour2 = self.ax[1].contourf(self.teos_range, self.ammonia_range, Z_water, levels=20, cmap='viridis')
+        self.fig.colorbar(contour2, ax=self.ax[1], label='AP_distance')
+        self.ax[1].set_xlabel('TEOS')
+        self.ax[1].set_ylabel('Ammonia')
+        self.ax[1].set_title('TEOS-Ammonia')
+
+        # Highlight the true minimum
+        self.ax[0].scatter(self.true_min[0], self.true_min[2], marker='*', color='pink')
+        self.ax[1].scatter(self.true_min[0], self.true_min[1], marker='*', color='pink')
+
+        self.fig.suptitle(self.trial_name)
+
+        # Text for batch label
+        self.text = self.ax[0].text(0.06, 0.135, '', fontsize=12, color="white", backgroundcolor="black")
+
+        # List to track scatter points for updating
+        self.scatter_plots = []
+
+    def get_batch_num(self, i):
+        if i < self.n_sobol:
+            return 0
+        else:
+            return int(np.ceil((i - self.n_sobol + 1)/self.batch_size))
+
+    def get_batch_splits(self):
+        ## Get batches
+
+        i = 0
+        n_sobol = 2**self.m_samples
+        batches_points = []
+        batch = []
+        for uuid_val, data_val in self.data.items():
+            point = [data_val['teos_vol_frac'], data_val['ammonia_vol_frac'], data_val['water_vol_frac']]
+            batch.append(point)
+                
+            if i == n_sobol -1:
+                batches_points.append(batch)
+                batch = []
+
+            if (i- (n_sobol - 1)) % self.batch_size == 0 and i > n_sobol - 1:
+                batches_points.append(batch)
+                batch = []
+
+            i += 1
+
+        return batches_points
+
+    def get_best_points(self):
+        best_points = {}
+
+        for i, (uuid_val, data_val) in enumerate(self.data.items()):
+            if uuid_val in self.best_uuid:
+                # get batch num:
+                batch_num = self.get_batch_num(i, self.n_sobol, self.batch_size)
+                point = [data_val['teos_vol_frac'], data_val['ammonia_vol_frac'], data_val['water_vol_frac']]
+                best_points[batch_num] = point    
+
+        return best_points    
+
+
+
+    def get_alpha(self, i, frame):
+        """Determines alpha transparency for previous points."""
+        if frame - i >= 5:
+            return 0.1
+        if frame - i - 1 == 0:
+            return 1
+        else:
+            return 0.2 * (frame - i)
+
+    def update(self, frame):
+        """Update function for animation."""
+
+        # Remove old scatter points
+        for sc in self.scatter_plots:
+            try:
+                sc.remove()
+            except:
+                continue
+        
+        # Add new points for each batch
+        for i in range(frame + 1):
+            self.text.set_text('Batch ' + self.batch_names[i])
+            alpha = self.get_alpha(i, frame)
+            current_batch = self.batches_points[i]
+            color = 'red' if frame - i - 1 == 0 else 'k'
+
+            for point in current_batch:
+                sc1 = self.ax[0].scatter(point[0], point[2], color=color, alpha=alpha)
+                sc2 = self.ax[1].scatter(point[0], point[1], color=color, alpha=alpha)
+                self.scatter_plots.append(sc1)
+                self.scatter_plots.append(sc2)
+
+            # Highlight best points in cyan
+            try:
+                bp = self.best_points[i]
+                self.ax[0].scatter(bp[0], bp[2], color='cyan', marker='*', s=100)
+                self.ax[1].scatter(bp[0], bp[1], color='cyan', marker='*', s=100)
+            except:
+                continue
+
+        return self.scatter_plots + [self.text]
+
+    def run(self):
+        """Run the animation and display it inline in Jupyter Notebook."""
+        self.ani = animation.FuncAnimation(self.fig, self.update, frames=self.num_frames, interval=800, blit=False)
+        return HTML(self.ani.to_jshtml())
+
+    def save(self, filename="animation.mp4", format="mp4"):
+        """Save the animation as a file."""
+        self.ani = animation.FuncAnimation(self.fig, self.update, frames=self.num_frames, interval=800, blit=False)
+
+        if format == "mp4":
+            self.ani.save(filename, writer=animation.FFMpegWriter(fps=10))
+        elif format == "gif":
+            self.ani.save(filename, writer=animation.PillowWriter(fps=10))
+        elif format == "html":
+            html_content = self.ani.to_jshtml()
+            with open(filename, "w") as f:
+                f.write(html_content)
+        else:
+            raise ValueError("Unsupported format. Use 'mp4', 'gif', or 'html'.")
+
+        print(f"Animation saved as {filename}")
---
+++ src/silica_opt_insilico/post_process.py
@@ -0,0 +1,236 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+import experiment
+import data_processing
+
+import pd_utils
+
+def composition_distance(measured_point, true_optima):
+    return np.sqrt(np.sum([(measured_point[i] - true_optima[i])**2 for i in range(len(measured_point))]))
+
+def process_distances(trial_name, params, true_min_composition, budget = 100, convergence_threshold = 0.05, fp = './'):
+    """
+    First pass through data
+    """
+
+    batch_size = params['batch_size']
+    amplitude_weight = params['amplitude_weight']
+    m_samples = params['m_samples']
+    lower_bounds = params['lower_bounds']
+    upper_bounds = params['upper_bounds']
+    noise_level = params['noise_level']
+
+        
+    if batch_size == 0:
+        n_batches = 0
+    else:
+        n_batches = int(np.ceil(budget/batch_size))
+
+    data_complete = {}
+    best_distances_list = []
+    best_uuids_list = []
+    converge_iterations = []
+    best_composition_dist = []
+    for i in range(3):
+        with open(f'{fp}{trial_name}_replicate_{i}.pkl', 'rb') as f:
+            data = pickle.load(f)
+        
+        name_bounds = {}
+        name_bounds['random'] = (0, 2**m_samples)
+        for i in range(n_batches):
+            name_bounds[f'Batch {i+1}'] = ((i)*batch_size + 2**m_samples, (i+1)*batch_size + 2**m_samples)
+
+        best_distances_ap = []
+        best_uuid = []
+        converge_its = None
+        
+        for i, (uuid_val, sample) in enumerate(data.items()):
+            data_complete[uuid_val] = sample
+            dist = sample['ap_distance_reporting']
+            if len(best_distances_ap) == 0:
+                best_distances_ap.append(dist)
+                best_distances_ap.append(dist)
+                best_uuid.append(uuid_val)
+            else:
+                best_distances_ap.append(min(best_distances_ap[-1], dist))
+        
+            
+            if dist < best_distances_ap[-2]:
+                #print('new min found')
+                best_uuid.append(uuid_val)
+
+            if dist < convergence_threshold:
+                converge_its = i
+                converge_iterations.append(i)
+        if converge_its is None:
+            converge_iterations.append('Not converged')
+            
+        best_distances_list.append(best_distances_ap)
+        best_uuids_list.append(best_uuid)
+
+        # get best composition distance
+        best_sample = data[best_uuid[-1]]
+        best_sample_comp = [best_sample['teos_vol_frac'], best_sample['ammonia_vol_frac'], best_sample['water_vol_frac']]
+        comp_dist = composition_distance(best_sample_comp, true_min_composition)
+        best_composition_dist.append(comp_dist)
+
+    return data_complete, best_distances_list, best_uuids_list, converge_iterations, best_composition_dist, name_bounds
+    
+
+def convergence_plot(data_complete, best_distances_list, best_uuids_list, name_bounds, trial_name, q_grid_nonlog, target_I):
+                         
+    fig, ax = plt.subplots()
+
+    for name, bounds in name_bounds.items():
+        ax.fill_between(bounds, 0, max(best_distances_ap), alpha = 0.2)
+    #    ax.text(np.mean(bounds), 0.1, name, rotation = 'vertical')
+
+    for best_distances_ap in best_distances_list:
+        ax.plot(best_distances_ap)
+    
+    
+    ax.set_xlabel('Sample number')
+    ax.set_ylabel('amplitude-phase distance')
+    ax.set_title(trial_name)
+    
+    plt.tight_layout()
+    #plt.savefig('Campaign_convergence_plot.png', dpi = 300)
+
+    return fig
+
+
+def best_scatterer_plots(data_complete, best_uuids_list, q_grid_nonlog, target_I, trial_name):
+
+    fig, ax = plt.subplots(1,len(best_uuids_list), figsize = (12,12/len(best_uuids_list)))
+
+    for i, best_uuid in enumerate(best_uuids_list):
+
+        I = data_complete[best_uuid[-1]]['I_scaled']
+    
+        ax[i].loglog(q_grid_nonlog, I)
+        ax[i].loglog(q_grid_nonlog, target_I, ls ='--')
+
+    fig.suptitle(trial_name)
+
+
+def phase_diagram(data, lower_bounds, upper_bounds, q_grid):
+    """
+    Phase plot, for 1 campaign
+    """
+
+    x_name = 'TEOS'
+    y_name = 'water'
+    y_2_name = 'ammonia'
+
+    x_min = lower_bounds[0]
+    x_max = upper_bounds[0]
+    y_min = lower_bounds[2]
+    y_max = upper_bounds[2]
+
+    y_2_min = lower_bounds[1]
+    y_2_max = upper_bounds[1]
+
+    bounds = np.array([[x_min, y_min], [x_max, y_max]])
+    bounds_2 = np.array([[x_min, y_2_min], [x_max, y_2_max]])
+
+    x_key = 'teos_vol_frac'
+    y_key = 'water_vol_frac'
+    y_2_key = 'ammonia_vol_frac'
+
+
+    fig, ax = plt.subplots(1,2, figsize = (12, 6))
+
+    c_1 = []
+    s = []
+    c_2 = []
+
+    colors_1 = []
+    colors_2 = []
+    for uuid_val, sample in data.items():
+
+        x_val = sample[x_key]
+        y_val = sample[y_key]
+        y_2_val = sample[y_2_key]
+
+        
+        I = np.log10(sample['scattering_I'])
+
+        c_1.append([x_val, y_val])
+        s.append(I)
+        c_2.append([x_val, y_2_val])
+        colors_1.append(get_colormap_color(y_2_val, y_2_min, y_2_max))
+        colors_2.append(get_colormap_color(y_val, y_min, y_max))
+
+            
+
+    s = np.array(s)
+
+    pd_utils.plot_phasemap(bounds, ax[0], c_1, s, colors = colors_1)
+    pd_utils.plot_phasemap(bounds_2, ax[1], c_2, s, colors = colors_2)
+
+
+    ax[0].set_xlabel('TEOS volume fraction')
+    ax[0].set_ylabel('Water volume fraction')
+    ax[1].set_xlabel('TEOS volume fraction')
+    ax[1].set_ylabel('Ammonia volume fraction')
+
+    cmap_name = 'viridis'
+    norm = mcolors.Normalize(vmin=y_2_min, vmax=y_2_max)
+    norm2 = mcolors.Normalize(vmin=y_min, vmax = y_max)
+    cmap = cm.get_cmap(cmap_name)
+    cmap2 = cm.get_cmap(cmap_name)
+
+    cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax[0])
+    cbar2 = fig.colorbar(cm.ScalarMappable(norm=norm2, cmap = cmap2), ax = ax[1])
+
+    cbar.set_label(y_2_key)
+    cbar2.set_label(y_key)
+
+
+    fig.suptitle('Round 2 Optimization Batch 3 - APdist')
+    #plt.savefig('Phaseplot_apdist_batch3_80nm.png', dpi = 300)
+    return fig
+
+def get_colormap_color(value, vmin, vmax, cmap_name='viridis'):
+    """
+    Maps a scalar value to an RGB color using a specified Matplotlib colormap.
+    
+    Parameters:
+        value (float): The scalar value to be mapped to a color.
+        vmin (float): The minimum bound of the scalar range.
+        vmax (float): The maximum bound of the scalar range.
+        cmap_name (str): The name of the Matplotlib colormap to use (default: 'viridis').
+    
+    Returns:
+        tuple: An (R, G, B) color tuple, where values are in the range [0, 1].
+    """
+    norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+    cmap = cm.get_cmap(cmap_name)
+    return cmap(norm(value))[:3] 
+
+
+
+
+### Animated contour plot
+
+def contour_eval(teos, ammonia, water, target_I, q_grid, return_scatter = False, amplitude_weight = 0.1):
+
+    #teos = sample[0]
+    #ammonia = sample[1]
+    #water = sample[2]
+
+    noise_level = 0
+    sample_point = (teos, ammonia, water)
+
+    scattering, real_sample_point, diameter, pdi = experiment.run_experiment(sample_point, noise_level, 10**q_grid, experiment.sld_silica, experiment.sld_etoh)
+
+    # Process measurement
+    ap_dist, ap_dist_report, I_scaled = data_processing.process_measurement(scattering, target_I, q_grid, amplitude_weight)
+
+    if return_scatter:
+        return ap_dist, scattering
+    else:
+        return ap_dist
